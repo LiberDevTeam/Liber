2093781837ca6eb03dc1e52fb1193a3774f98b94
Ryo Narita <mail.ryonarita@gmail.com>
2021-01-30 17:11:24 +0900

    Update themes

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,57 @@
+import { css } from 'styled-components';
+
 const colors = {
-  main: '#2e79f6',
+  primary: '#2e79f6',
+  primaryText: '#1a2c58',
+  secondaryText: '#a0aabe',
+  lightText: '#fcfdfe',
+  bg: '#fcfdfe',
+  bg2: '#f0f5fa',
+  bg3: '#f3f3f3',
+  green: '#4eeb91',
+  yellow: '#e8eb5c',
+  red: '#ef4141',
+};
+
+const fontSizes = {
+  xs: '0.75rem',
+  sm: '0.875rem',
+  md: '1rem',
+  lg: '1.125rem',
+  xl: '1.25rem',
+  '2xl': '1.5rem',
+  '3xl': '1.875rem',
+  '4xl': '2.25rem',
+  '5xl': '3rem',
+  '6xl': '3.75rem',
+  '7xl': '4.5rem',
+  '8xl': '6rem',
+  '9xl': '8rem',
+};
+const fontWeights = {
+  thin: 100,
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+  black: 900,
 };
 
 export const theme = {
   colors,
+  fontWeights,
+  fontSizes,
+  space: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64],
+  sizes: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64],
+  shadows: {
+    0: 'none',
+    1: '0px 5px 20px rgba(0, 0, 0, 0.2)',
+  },
+  radii: {
+    medium: 12,
+    large: 20,
+    round: '50%',
+  },
 };
