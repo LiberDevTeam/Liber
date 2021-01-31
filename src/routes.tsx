3e8a1132a144fe0f5d582bcb716cadc94bd8a129
mecaota <mecaota@users.noreply.github.com>
2021-01-31 21:14:11 +0900

    routes.tsxからレイアウト要素をBaseLayoutへ移設

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,37 +4,15 @@ import { Route, Switch } from 'react-router-dom';
 import IndexPage from './pages';
 import { NewPlace } from './pages/places/new';
 import { history } from './state/store';
-import { SideNavigation } from '~/components/side-navigation';
-import styled from 'styled-components';
 import { Chats } from './pages/chats';
-
-const Root = styled.div`
-  display: grid;
-  grid-template-columns: 220px auto;
-  grid-gap: ${(props) => props.theme.space[5]}px;
-  padding: ${(props) => props.theme.space[5]}px;
-  height: 100%;
-`;
-
-const Main = styled.div`
-  background: ${(props) => props.theme.colors.bg};
-  border-radius: ${(props) => props.theme.radii.large}px;
-  padding: ${(props) => props.theme.space[8]}px;
-  overflow: hidden;
-`;
 
 export const Routes: React.FC = () => (
   <ConnectedRouter history={history}>
-    <Root>
-      <SideNavigation />
-      <Main>
-        {/* your usual react-router-dom v4/v5 routing */}
-        <Switch>
-          <Route exact path="/" render={() => <IndexPage />} />
-          <Route exact path="/places/new" render={() => <NewPlace />} />
-          <Route path="/chats/:cid?" render={() => <Chats />} />
-        </Switch>
-      </Main>
-    </Root>
+    {/* your usual react-router-dom v4/v5 routing */}
+    <Switch>
+      <Route exact path="/" render={() => <IndexPage />} />
+      <Route exact path="/places/new" render={() => <NewPlace />} />
+      <Route path="/chats/:cid?" render={() => <Chats />} />
+    </Switch>
   </ConnectedRouter>
 );
--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,25 +1,33 @@
-import React, { useEffect } from 'react';
-import { selectMe } from '~/state/ducks/me/meSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { connect } from '~/connection';
-import Navbar from '~/components/organisms/navbar';
+import React from 'react';
+import styled from 'styled-components';
+import { SideNavigation } from '~/components/side-navigation';
+
+const Root = styled.div`
+  display: grid;
+  grid-template-columns: 220px auto;
+  grid-gap: ${(props) => props.theme.space[5]}px;
+  padding: ${(props) => props.theme.space[5]}px;
+  height: 100%;
+`;
+
+const Header = styled.header``;
+
+const Main = styled.main`
+  background: ${(props) => props.theme.colors.bg};
+  border-radius: ${(props) => props.theme.radii.large}px;
+  padding: ${(props) => props.theme.space[8]}px;
+  overflow: hidden;
+`;
 
 const BaseLayout: React.FC = ({ children }) => {
-  const dispatch = useDispatch();
-  const me = useSelector(selectMe);
-
-  // TODO use server notification to trigger to connect.
-  useEffect(() => {
-    dispatch(connect(`ws://192.168.10.101:3000/ws?uid=${me.id}`, me));
-  }, []);
-
   return (
-    <div>
-      <header className="App-header">
-        <Navbar channels={me.channels} />
-      </header>
-      <main>{children}</main>
-    </div>
+    <Root>
+      <Header>
+        <SideNavigation />
+      </Header>
+
+      <Main>{children}</Main>
+    </Root>
   );
 };
 
