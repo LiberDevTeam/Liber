0e289ff528ebcb272a12bf9f0d1d9fb3107cced6
Ryo Narita <mail.ryonarita@gmail.com>
2021-02-07 01:41:58 +0900

    feat: Create message view component (#37)

---
+++ src/components/molecules/message-view/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MessageView, MessageViewProps } from '.';
+import { Story } from '@storybook/react/types-6-0';
+
+export default {
+  component: MessageView,
+  title: 'molecules/MessageView',
+};
+
+const Template: Story<MessageViewProps> = (args) => <MessageView {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  uid: 'C7140B81',
+  text: 'それはpornスター。サッカーやない',
+  timestamp: 1612204980858,
+};
