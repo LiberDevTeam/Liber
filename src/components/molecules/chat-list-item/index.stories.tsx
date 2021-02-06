00eaf87c281b9669a33621e0825c9d369635c180
mecaota <24618982+mecaota@users.noreply.github.com>
2021-02-06 23:03:24 +0900

    storyファイルからRouterを削除して、preview.jsでRouterをラップ

--- src/components/molecules/chat-list-item/index.stories.tsx
+++ src/components/molecules/chat-list-item/index.stories.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { ChatListItem, ChatListItemProps } from '.';
 import { Story } from '@storybook/react/types-6-0';
-import { createBrowserHistory } from 'history';
-import { Router } from 'react-router-dom';
 
 export default {
   component: ChatListItem,
   title: 'molecules/ChatListItem',
 };
 
-const history = createBrowserHistory();
-
-const Template: Story<ChatListItemProps> = (args) => (
-  <Router history={history}>
-    <ChatListItem {...args} />
-  </Router>
-);
+const Template: Story<ChatListItemProps> = (args) => <ChatListItem {...args} />;
 export const Default = Template.bind({});
--- src/components/organisms/side-navigation/index.stories.tsx
+++ src/components/organisms/side-navigation/index.stories.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { SideNavigation } from '.';
 import { Story } from '@storybook/react/types-6-0';
-import { createBrowserHistory } from 'history';
-import { Router } from 'react-router-dom';
 
 export default {
   component: SideNavigation,
   title: 'organisms/SideNavigation',
 };
 
-const history = createBrowserHistory();
-
-const Template: Story = (args) => (
-  <Router history={history}>
-    <SideNavigation {...args} />
-  </Router>
-);
+const Template: Story = (args) => <SideNavigation {...args} />;
 export const Default: Story = Template.bind({});
