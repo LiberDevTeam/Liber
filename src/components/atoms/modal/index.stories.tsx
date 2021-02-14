901db9b1339494071dffe6c651d8e850ff434c70
Ryo Narita <mail.ryonarita@gmail.com>
2021-02-15 03:18:20 +0900

    feat: implement modal and share url dialog (#49)

---
+++ src/components/atoms/modal/index.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Modal, ModalProps } from './';
+import { Story } from '@storybook/react/types-6-0';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
+
+export default {
+  component: Modal,
+  title: 'atoms/Modal',
+  argTypes: {
+    onClose: {
+      action: 'onClose',
+    },
+    children: {
+      control: false,
+    },
+  },
+};
+
+const Template: Story<ModalProps> = (args) => <Modal {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  open: true,
+  title: 'Modal title',
+  children: <div style={{ width: 572 }}>content</div>,
+};
