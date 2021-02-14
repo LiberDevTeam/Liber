e60c25d49394b2986ad3188401c47a5e20991381
mecaota <24618982+mecaota@users.noreply.github.com>
2021-02-14 21:09:15 +0900

    place-detailのstoryファイルにモック追加

--- src/components/organisms/place-detail/index.stories.tsx
+++ src/components/organisms/place-detail/index.stories.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Story } from '@storybook/react/types-6-0';
 import { PlaceDetail, PlaceDetailProps } from '.';
+import { PlaceItem } from '~/components/molecules/place-list-item';
+import { getUnixTime } from 'date-fns';
+import { Message } from '~/state/ducks/place/placeSlice';
+// TODO: 実際のchatデータモックに置き換える
+const id = '1';
+const place: PlaceItem = {
+  id,
+  title: 'We Love FC Barcelona!!',
+  avatarImage: `https://i.pravatar.cc/60?u=${id}`,
+  description:
+    'this is the last message someone saidasdjfl;askjd;flkajsd;flkjasd;lkfj;dlskaj',
+  timestamp: getUnixTime(new Date()),
+};
+const message: Message = {
+  id,
+  type: 'Text',
+  uid: id,
+  timestamp: getUnixTime(new Date()),
+  text:
+    'this is the last message someone saidasdjfl;askjd;flkajsd;flkjasd;lkfj;dlskaj',
+};
 
 export default {
   component: PlaceDetail,
   title: 'organisms/PlaceDetail',
+  args: {
+    place,
+    messages: [...new Array(10)].map(() => message),
+  },
 };
 
 const Template: Story<PlaceDetailProps> = (args) => <PlaceDetail {...args} />;
