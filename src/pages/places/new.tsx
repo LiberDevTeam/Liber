87001e324e9f5028b1c657f25b49a4d9ab2b441d
Ryo Narita <mail.ryonarita@gmail.com>
2021-02-15 22:53:08 +0900

    feat: add avatar image uploading (#58)

--- src/pages/places/new.tsx
+++ src/pages/places/new.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PageTitle } from '~/components/atoms/page-title';
 import styled from 'styled-components';
 import BaseLayout from '~/templates';
@@ -36,9 +36,40 @@ const PrivateFlagGroup = styled.div`
   margin-top: ${(props) => props.theme.space[5]}px;
 `;
 
+const InputFile = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0;
+`;
+const UploadFileButtonGroup = styled.div`
+  position: relative;
+  &:active {
+    opacity: 0.8;
+  }
+`;
+const PreviewImage = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: ${(props) => props.theme.radii.medium}px;
+`;
+
 const SubmitButton = styled(Button)`
   margin-top: ${(props) => props.theme.space[8]}px;
 `;
+
+const readFile = (file: Blob) =>
+  new Promise<string>((resolve) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      if (e.target && e.target.result) {
+        resolve(e.target.result as string);
+      }
+    };
+    reader.readAsDataURL(file);
+  });
 
 interface FormValues {
   name: string;
@@ -48,6 +79,8 @@ interface FormValues {
 
 export const NewPlace: React.FC = React.memo(function NewPlace() {
   const dispatch = useDispatch();
+  const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
+  const [avatarImage, setAvatarImage] = useState<File | null>(null);
   const formik = useFormik<FormValues>({
     initialValues: {
       name: '',
@@ -68,6 +101,14 @@ export const NewPlace: React.FC = React.memo(function NewPlace() {
       dispatch(push(`/places/${id}`));
     },
   });
+
+  const handleChangeImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      setAvatarImage(e.currentTarget.files[0]);
+      const file = await readFile(e.currentTarget.files[0]);
+      setAvatarPreview(file);
+    }
+  };
 
   return (
     <BaseLayout>
@@ -90,6 +131,17 @@ export const NewPlace: React.FC = React.memo(function NewPlace() {
           disabled={formik.isSubmitting}
         />
 
+        {avatarPreview ? <PreviewImage src={avatarPreview} /> : null}
+        <UploadFileButtonGroup>
+          <Button text="Select Thumbnail Image" shape="square" type="button" />
+          <InputFile
+            name="avatarImage"
+            type="file"
+            accept="image/*"
+            onChange={handleChangeImage}
+          />
+        </UploadFileButtonGroup>
+
         <PrivateFlagGroup role="group">
           <label>
             <input
@@ -107,7 +159,7 @@ export const NewPlace: React.FC = React.memo(function NewPlace() {
           text="Submit"
           variant="solid"
           type="submit"
-          disabled={formik.isSubmitting}
+          disabled={formik.isSubmitting || formik.isValid === false}
         />
       </Form>
     </BaseLayout>
