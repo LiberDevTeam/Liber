ec5b29ebee5db5dd2c17a69b364a78f227bb054c
mecaota <24618982+mecaota@users.noreply.github.com>
2021-02-21 00:31:11 +0900

    toggleSwitchコンポーネントを実装

---
+++ src/components/atoms/toggle-switch/index.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ToggleSwitch, ToggleSwitchProps } from '.';
+import { Story } from '@storybook/react/types-6-0';
+
+export default {
+  component: ToggleSwitch,
+  title: 'atoms/ToggleSwitch',
+  argTypes: {
+    checked: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    width: {
+      control: {
+        type: 'range',
+        min: 1,
+        max: 1000,
+      },
+    },
+  },
+};
+
+const Template: Story<ToggleSwitchProps> = (args) => <ToggleSwitch {...args} />;
+export const Default = Template.bind({});
+Default.args = { width: 50 };
