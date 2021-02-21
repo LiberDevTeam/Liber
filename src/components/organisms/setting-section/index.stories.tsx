3a2c134a81688e8464ec06951c7af0c0126b59d3
mecaota <24618982+mecaota@users.noreply.github.com>
2021-02-21 22:30:38 +0900

    setting-sectionコンポーネントを実装

---
+++ src/components/organisms/setting-section/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story } from '@storybook/react/types-6-0';
+import { SettingSection, SettingSectionProps } from '.';
+
+export default {
+  component: SettingSection,
+  title: 'organisms/SettingSection',
+  args: {
+    title: 'title',
+    description: 'description',
+  },
+};
+
+const Template: Story<SettingSectionProps> = (args) => (
+  <SettingSection {...args} />
+);
+export const Default = Template.bind({});
---
+++ src/components/organisms/setting-section/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  width: 100%;
+`;
+
+const SectionTitle = styled.p`
+  font-size: ${(props) => props.theme.fontSizes.md};
+  font-weight: ${(props) => props.theme.fontWeights.medium};
+  color: ${(props) => props.theme.colors.secondaryText};
+`;
+
+const SectionDescription = styled.p`
+  font-size: ${(props) => props.theme.fontSizes.xs};
+  font-weight: ${(props) => props.theme.fontWeights.normal};
+  color: ${(props) => props.theme.colors.secondaryText};
+`;
+
+const SectionMain = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: ${(props) => props.theme.space[3]}px 0;
+`;
+
+export type SettingSectionProps = {
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+};
+
+export const SettingSection: React.FC<SettingSectionProps> = ({
+  title,
+  description = '',
+  children,
+}) => {
+  return (
+    <Container>
+      <SectionTitle>{title}</SectionTitle>
+      <SectionDescription>{description}</SectionDescription>
+      <SectionMain>{children}</SectionMain>
+    </Container>
+  );
+};
