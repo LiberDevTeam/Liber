3e249731722ca9618338c6861250b878f0ff47de
Ryo Narita <mail.ryonarita@gmail.com>
2021-02-28 23:45:13 +0900

    Make it possible to remove avatar file from form (#75)

--- src/components/atoms/icon-button/index.tsx
+++ src/components/atoms/icon-button/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { lighten } from 'polished';
 
 const Root = styled.button`
   display: inline-flex;
@@ -9,20 +10,19 @@ const Root = styled.button`
   background: transparent;
   padding: 0;
   border-radius: ${(props) => props.theme.radii.medium}px;
+  color: ${(props) => props.theme.colors.primary};
 
   &:focus {
     box-shadow: ${(props) => props.theme.colors.lightPrimary} 0px 0px 0px 2px;
     outline: none;
   }
 
-  &:active {
-    svg {
-      opacity: 0.6;
-    }
+  &:hover {
+    color: ${(props) => lighten(0.1, props.theme.colors.primary)};
   }
 
-  &:hover {
-    opacity: 0.8;
+  &:active {
+    color: ${(props) => lighten(0.2, props.theme.colors.primary)};
   }
 `;
 
---
+++ src/components/molecules/preview-image/index.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { PreviewImage, PreviewImageProps } from '.';
+import { Story } from '@storybook/react/types-6-0';
+import LogoSVG from '../../../logo.svg';
+
+export default {
+  component: PreviewImage,
+  title: 'molecules/PreviewImage',
+  argTypes: {
+    onRemove: {
+      action: 'onRemove',
+    },
+  },
+};
+
+const Template: Story<PreviewImageProps> = (args) => <PreviewImage {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  src: LogoSVG,
+};
