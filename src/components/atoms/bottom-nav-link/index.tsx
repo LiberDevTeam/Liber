ea6712625d5688b9e7548fd2d923e05bcf79f898
Koya Fukushi <fukushi098@gmail.com>
2021-03-15 00:04:14 +0900

    neat sp tabbar

--- src/components/atoms/bottom-nav-link/index.tsx
+++ src/components/atoms/bottom-nav-link/index.tsx
@@ -11,8 +11,9 @@ const StyledNavLink = styled(NavLink)`
   width: 100%;
   height: 100%;
   color: ${(props) => props.theme.colors.secondaryText};
-  font-size: ${(props) => props.theme.fontSizes.sm};
   text-decoration: none;
+  font-weight: ${(props) => props.theme.fontWeights.light};
+  font-size: ${(props) => props.theme.fontSizes.xs};
 
   &.${activeClassName} {
     svg {
