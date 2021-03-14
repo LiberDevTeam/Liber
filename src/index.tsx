b67aaa9417c9b1abb79241d8099f0b42c4507ffd
mecaota <24618982+mecaota@users.noreply.github.com>
2021-03-14 23:07:56 +0900

    ロケール対応 (#87)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '~/lib/i18n';
 import { store } from './state/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
