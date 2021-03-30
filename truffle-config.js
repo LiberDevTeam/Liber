69b7633410bfc0f9908073bdc2d5218571c891e6
mecaota <24618982+mecaota@users.noreply.github.com>
2021-03-30 18:53:30 +0900

    build develop environment typescript truffle

--- truffle-config.js
+++ truffle-config.js
@@ -24,7 +24,13 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+//require("ts-node/register");
+require("ts-node").register({
+  files: true,
+});
+
 module.exports = {
+  test_file_extension_regexp: /.*\.ts$/,
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
