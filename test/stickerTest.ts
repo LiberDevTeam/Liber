81a61226fd2b0e0fe9e469208bddedcf749cea38
mecaota <24618982+mecaota@users.noreply.github.com>
2021-03-31 23:29:09 +0900

    develop firstDemoToken publish test

--- test/stickerTest.ts
+++ test/stickerTest.ts
@@ -1,14 +1,13 @@
+const MAX_OF_UINT256 = web3.utils.toBN(2).pow(web3.utils.toBN(255)).toString();
 const LiberSticker = artifacts.require("LiberSticker");
-
-const MAX_OF_UINT256 = 2**256;
 
 contract("LiberSticker", accounts => {
   it("HELLO Sticker is published max of systems", () =>
     LiberSticker.deployed()
-      .then(instance => instance.balanceOf(accounts[0], 1))
+      .then(instance => instance.balanceOf(accounts[0], 0))
       .then(balance => {
         assert.equal(
-          balance.valueOf(),
+          balance.toString(),
           MAX_OF_UINT256,
           "HELLO sticker published in the first account"
         );
