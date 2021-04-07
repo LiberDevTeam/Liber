5e7641da3bec9d7b915fc1520f2320c8592b8a75
mecaota <24618982+mecaota@users.noreply.github.com>
2021-04-07 18:59:12 +0900

    change token variables to "YO"

--- contracts/LiberSticker.sol
+++ contracts/LiberSticker.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
 contract LiberSticker is ERC1155PresetMinterPauser {
-    uint256 public constant HELLO = 0;
+    uint256 public constant YO = 0;
 
     constructor() ERC1155PresetMinterPauser("https://jsonplaceholder.typicode.com/posts/{id}") {
         // todo: default sticker minting
-        publishSticker(HELLO);
+        publishSticker(YO);
     }
 
     // publish sticker Token, amount of 2**255
