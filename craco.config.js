60c6fdec65d8090052a52aa3b7a954dde57509c7
Ryo Narita <mail.ryonarita@gmail.com>
2021-04-11 00:39:02 +0900

    Setup service worker

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const WorkerPlugin = require('worker-plugin');
 module.exports = {
   plugins: [
     {
       plugin: {
         overrideWebpackConfig: ({ webpackConfig }) => {
           webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+          webpackConfig.plugins.push(new WorkerPlugin());
           return webpackConfig;
         },
       },
