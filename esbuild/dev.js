2444744f26f0d2d2f65724c7cfad67a85af41e69
Koya Fukushi <fukushi098@gmail.com>
2021-04-19 00:11:51 +0900

    https

--- esbuild/dev.js
+++ esbuild/dev.js
@@ -1,11 +1,52 @@
+const path = require('path');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const { commonConfig } = require('./config');
 
 const PORT = 3000;
 
 require('esbuild')
-  .serve({ servedir: 'public', port: PORT }, commonConfig)
-  .then(() => {
-    console.log(`Run server at http://localhost:${PORT}`);
+  .serve({ servedir: 'public' }, commonConfig)
+  .then((result) => {
+    console.log(`Run server at https://localhost:${PORT}`);
+
+    // The result tells us where esbuild's local server is
+    const { host, port } = result;
+    const options = {
+      key: fs.readFileSync(path.join(__dirname, 'server_key.pem')),
+      cert: fs.readFileSync(path.join(__dirname, 'server_cert.pem')),
+    };
+
+    // Then start a proxy server on port 3000
+    https
+      .createServer(options, (req, res) => {
+        const options = {
+          hostname: host,
+          port: port,
+          path: req.url,
+          method: req.method,
+          headers: req.headers,
+        };
+
+        // Forward each incoming request to esbuild
+        const proxyReq = http.request(options, (proxyRes) => {
+          // If esbuild returns "not found", send a custom 404 page
+          if (proxyRes.statusCode === 404) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<h1>A custom 404 page</h1>');
+            return;
+          }
+
+          // Otherwise, forward the response from esbuild to the client
+          res.writeHead(proxyRes.statusCode, proxyRes.headers);
+          proxyRes.pipe(res, { end: true });
+        });
+
+        // Forward the body of the request to esbuild
+        req.pipe(proxyReq, { end: true });
+      })
+      .listen(PORT);
   })
   .catch((e) => {
     console.error(e);
