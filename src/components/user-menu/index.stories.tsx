dfdb26f259c2d56320401a939bf8d16cc523d02d
Ryo Narita <mail.ryonarita@gmail.com>
2021-05-14 18:55:50 +0900

    Update index.stories.tsx

--- src/components/user-menu/index.stories.tsx
+++ src/components/user-menu/index.stories.tsx
@@ -1,10 +1,10 @@
 import { Story } from '@storybook/react/types-6-0';
-import { UserMenu } from './';
+import { UserMenu, UserMenuProps } from './';
 
 export default {
   component: UserMenu,
   title: 'molecules/UserMenu',
 };
 
-const Template: Story = (args) => <UserMenu {...args} />;
+const Template: Story<UserMenuProps> = (args) => <UserMenu {...args} />;
 export const Default = Template.bind({});
