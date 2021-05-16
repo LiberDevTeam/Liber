6a893dbc6ad6f9739585db5e46e8cf739dc13b82
mecaota <24618982+mecaota@users.noreply.github.com>
2021-05-16 18:48:37 +0900

    market contract and test deploy

--- contracts/LiberMarket.sol
+++ contracts/LiberMarket.sol
@@ -1,14 +1,103 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0;
 
-contract LiberMarket {
-  mapping(uint256=>uint256) private marketItems; // token_id to price struct
-  
-  function sellSticker(uint256 id, uint256 price) public {
-    marketItems[id] = price;
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract LiberMarket is Ownable, Pausable, PullPayment {
+  struct MarketItem {
+    ERC1155 token;
+    uint256 tokenId;
+    address publisher;
+    uint256 price;
+    bool isSale;
+  }
+  MarketItem[] private _marketItems; // token_id to price struct
+  mapping(address=>uint256[]) private itemsByPublsiher; // token_id to owner address map
+
+  constructor() Ownable(){
+    emit Test(address(this));
+  }
+
+  // token publish event
+  event ListToken(address indexed owner, uint256 indexed itemId);
+  event Test(address adrs);
+
+  // list the sticker on the market
+  function listToken(ERC1155 token, uint256 tokenId, uint256 price, bool isSale) public {
+    address publisher = msg.sender;
+    address market = address(this);
+    uint256 amount = token.balanceOf(publisher, tokenId);
+
+    require(
+      token.isApprovedForAll(publisher, market),
+      "Publisher must approve that contract operate"
+    );
+    require(
+      1 < amount,
+      "Operator has not own token to list"
+    );
+
+    _marketItems.push(MarketItem(token, tokenId, publisher, price, isSale));
+    uint256 itemId = _marketItems.length - 1;
+    itemsByPublsiher[publisher].push(itemId);
+    if(isSale) {
+      emit ListToken(publisher, itemId);
+    }
+  }
+
+/*
+  // unlist the sticker from the market
+  function unlistToken(uint256 id) public {
+    address publisher = msg.sender;
+    address market = address(this);
+    uint256 amount = _tokenContract.balanceOf(market, id);
+
+    require(
+      _tokenContract.isApprovedForAll(market, publisher),
+      "Market must approve that contract operate"
+    );
+    require(
+      _tokenContract.getPublisher(id) == publisher,
+      "Operator is not publisher"
+    );
+    require(
+      marketItems[id].publisher == publisher,
+      "This token has not listed"
+    );
+
+    _tokenContract.safeTransferFrom(market, publisher, id, amount, "");
+    marketItems[id] = MarketItem(address(0), 0, false);
+  }
+  */
+
+  // set sale flag
+  function setSale(uint256 id, bool isSale) public {
+    address publisher = msg.sender;
+
+    require(
+      _marketItems[id].publisher == publisher,
+      "Operater is not publisher"
+    );
+
+    _marketItems[id].isSale = isSale;
+  }
+
+  // set price
+  function setPrice(uint256 id, uint256 price) public {
+    address publisher = msg.sender;
+
+    require(
+      _marketItems[id].publisher == publisher,
+      "Operater is not publisher"
+    );
+
+    _marketItems[id].price = price;
   }
 
   function getPriceByToken(uint256 id) public view returns(uint256) {
-    return marketItems[id];
+    return _marketItems[id].price;
   }
 }
--- test-ts/sticker.ts
+++ test-ts/sticker.ts
@@ -1,5 +1,6 @@
 const MAX_OF_UINT256 = web3.utils.toBN(2).pow(web3.utils.toBN(255));
 const LiberSticker = artifacts.require("LiberSticker");
+const LiberMarket = artifacts.require("LiberMarket");
 
 // Sticker tests
 contract("LiberSticker", (accounts) => {
@@ -96,5 +97,55 @@ contract("LiberSticker", (accounts) => {
       operator,
       "Publisher is operator"
     );
+  });
+});
+
+contract("LiberMarket", (accounts) => {
+  const [operator, receiver] = accounts;
+  const txDetails: Truffle.TransactionDetails = {
+    from: operator
+  };
+  it("should list token to market", async() => {
+    const stickerInstance = await LiberSticker.new(txDetails);
+    const marketInstance = await LiberMarket.new(txDetails);
+    const stickerAddress = stickerInstance.address;
+    const marketAddress = marketInstance.address;
+
+    // token publish
+    const tokenId = async() => {
+      const events = await stickerInstance.publishNewSticker();
+      const filteredEvents = events.logs.filter((log)=>log.event === "PublishToken");
+      return filteredEvents.reduce((accm: string, log) => {
+        if ('tokenId' in log.args) {
+          return log.args.tokenId.toString();
+        }
+        return accm;
+      }, '');
+    };
+
+    // approve to translate token for market contract
+    const approvedOperator = async() => {
+      const events = await stickerInstance.setApprovalForAll(marketAddress, true);
+      const filteredEvents = events.logs.filter((log)=>log.event === "ApprovalForAll");
+      return filteredEvents.reduce((accm: string, log) => {
+        if ('operator' in log.args) {
+          return log.args.operator.toString();
+        }
+        return accm;
+      }, '');
+    };
+    assert.isOk(await approvedOperator(), 'Approve to translate token for market');
+
+    const itemId = async() => {
+      const events = await marketInstance.listToken(stickerAddress, await tokenId(), 100, true);
+      const filteredEvents = events.logs.filter((log)=>log.event === "ListToken");
+      return filteredEvents.reduce((accm: string, log) => {
+        if ('itemId' in log.args) {
+          return log.args.itemId.toString();
+        }
+        return accm;
+      }, '');
+    };
+    assert.isOk(await itemId(), 'Listed in market');
   });
 });
