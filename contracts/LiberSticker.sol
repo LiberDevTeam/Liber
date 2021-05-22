57dd8b9e63329cd019d39b8a5a7bcf2274396365
mecaota <24618982+mecaota@users.noreply.github.com>
2021-05-22 18:40:40 +0900

    change logic  token publisher identification

--- contracts/LiberSticker.sol
+++ contracts/LiberSticker.sol
@@ -4,57 +4,88 @@ pragma solidity >=0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
 contract LiberSticker is ERC1155PresetMinterPauser {
+    bytes32 public constant BANNED_ROLE = keccak256("banned");
+    string private constant SALT = "liberstickertoken";
     uint256 private _newTokenId = 0;
     address private marketAddress;
-    mapping(uint256=>address) private _tokenPublishers; // token_id to owner address map
 
     // token publish event
     event PublishToken(address indexed owner, uint256 indexed tokenId, uint256 amount);
     // token buying request event
     event TokenPurdchaseRequest(address indexed buyer, uint256 indexed tokenId);
 
+    modifier notBanned() {
+        address operator = msg.sender;
+        require(
+            !hasRole(BANNED_ROLE, operator),
+            "Operater is banned"
+        );
+        _;
+    }
+
     // todo: change template uri to production
-    constructor() ERC1155PresetMinterPauser("https://jsonplaceholder.typicode.com/posts/{id}") {
-        //marketAddress = contractAddress;
-        // todo: default sticker minting
-        publishNewSticker();
+    constructor(address market) ERC1155PresetMinterPauser("https://jsonplaceholder.typicode.com/posts/{id}"){
+        marketAddress = market;
+        _setupRole(DEFAULT_ADMIN_ROLE, marketAddress);
+        _setupRole(PAUSER_ROLE, marketAddress);
     }
 
     // get sticker publisher
     function getPublisher(uint256 id) public view returns (address) {
-        return _tokenPublishers[id];
+        bytes32 role = _generateRoleName(id);
+        uint256 roleMemberCount = getRoleMemberCount(role);
+        return getRoleMember(role, roleMemberCount);
     }
 
-    // publish sticker Token base function
-    function publishSticker(uint256 id) public {
+    // publish sticker Token up to amount of 2**255
+    function publishSticker(uint256 id) public notBanned {
         address operator = msg.sender;
-        uint256 tokenId;
-        if(id == 0) {
-            tokenId = _newTokenId;
-            _newTokenId++; // increment tokenId
-        }
-        uint256 amount = 2**255 - super.balanceOf(operator ,tokenId);
+        uint256 amount = 2**255 - balanceOf(operator, id);
 
         require(
             operator != address(0),
             "Operater is zero address"
         );
         require(
+            hasRole(_generateRoleName(id), operator),
+            "You are not this token publisher"
+        );
+        require(
             0 < amount,
             "Token have been published max of amount"
         );
-        require(
-            _tokenPublishers[tokenId] == address(0) || _tokenPublishers[tokenId] == operator,
-            "Token has already published, and you are not this token publisher"
-        );
 
-        super._mint(operator, tokenId, amount, "");
-        _tokenPublishers[tokenId] = operator;
-        emit PublishToken(operator, tokenId, amount);
+        _mint(operator, id, amount, "");
+        emit PublishToken(operator, id, amount);
     }
 
-    // publish sticker Token, amount of 2**255
-    function publishNewSticker() public {
-        publishSticker(0);
+    // publish new sticker Token
+    function publishNewSticker() public notBanned {
+        address operator = msg.sender;
+        uint256 id = _newTokenId; // numbering tokenId
+        bytes32 role = _generateRoleName(id);
+        require(
+            getRoleMemberCount(role) <= 0,
+            "Only grant token publish role for non published token"
+        );
+        grantRole(role, operator);
+        publishSticker(id);
+        _newTokenId++; // increment tokenId
+    }
+
+    // approve for market
+    function setApprovalForMarket(bool approved) public {
+        setApprovalForAll(marketAddress, approved);
+    }
+
+    function _generateRoleName(uint256 id) private pure returns(bytes32) {
+        bytes memory temp = new bytes(id);
+        bytes memory saltBytes = bytes(SALT);
+        for(uint256 i = 0; i < temp.length; i++) {
+            if(i < saltBytes.length) {
+                temp[i] = saltBytes[i];
+            }
+        }
+        return keccak256(temp);
     }
 }
