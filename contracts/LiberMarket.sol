5aa53474d8c0013516e5045a23e96bf1896b8426
mecaota <24618982+mecaota@users.noreply.github.com>
2021-05-24 22:21:48 +0900

    develop market

--- contracts/LiberMarket.sol
+++ contracts/LiberMarket.sol
@@ -2,37 +2,70 @@
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract LiberMarket is Ownable, Pausable, PullPayment {
+contract LiberMarket is AccessControlEnumerable, PullPayment, ReentrancyGuard, Pausable {
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+  bytes32 public constant BANNED_ROLE = keccak256("BANNED_ROLE");
   struct MarketItem {
     ERC1155 token;
     uint256 tokenId;
-    address publisher;
+    address listedOwner;
     uint256 price;
     bool isSale;
   }
   MarketItem[] private _marketItems; // token_id to price struct
   mapping(address=>uint256[]) private itemsByPublsiher; // token_id to owner address map
 
-  constructor() Ownable(){
-    emit Test(address(this));
+  constructor() AccessControlEnumerable() PullPayment() ReentrancyGuard() Pausable() {
+    address operator = msg.sender;
+    _setupRole(DEFAULT_ADMIN_ROLE, operator);
+    _setupRole(PAUSER_ROLE, operator);
   }
 
   // token publish event
-  event ListToken(address indexed owner, uint256 indexed itemId);
-  event Test(address adrs);
+  event ListItem(uint256 indexed itemId, address indexed listedOwner, uint256 price);
+  event UnListItem(uint256 indexed itemId);
+
+  modifier notBanned() {
+    address operator = msg.sender;
+    require(
+      !hasRole(BANNED_ROLE, operator),
+      "Operater is banned"
+    );
+    _;
+  }
+
+  modifier onlyPauser() {
+    address operator = msg.sender;
+    require(
+      hasRole(PAUSER_ROLE, operator),
+      "Operater is not allowed pause"
+    );
+    _;
+  }
+
+  // pause market
+  function pause() public whenNotPaused onlyPauser {
+    _pause();
+  }
+
+  // unpause market
+  function unPause() public whenPaused onlyPauser {
+    _unpause();
+  }
 
   // list the sticker on the market
-  function listToken(ERC1155 token, uint256 tokenId, uint256 price, bool isSale) public {
-    address publisher = msg.sender;
+  function listToken(ERC1155 token, uint256 tokenId, uint256 price, bool isSale) public whenNotPaused notBanned {
+    address listedOwner = msg.sender;
     address market = address(this);
-    uint256 amount = token.balanceOf(publisher, tokenId);
+    uint256 amount = token.balanceOf(listedOwner, tokenId);
 
     require(
-      token.isApprovedForAll(publisher, market),
+      token.isApprovedForAll(listedOwner, market),
       "Publisher must approve that contract operate"
     );
     require(
@@ -40,46 +73,56 @@ contract LiberMarket is Ownable, Pausable, PullPayment {
       "Operator has not own token to list"
     );
 
-    _marketItems.push(MarketItem(token, tokenId, publisher, price, isSale));
+    _marketItems.push(MarketItem(token, tokenId, listedOwner, price, isSale));
     uint256 itemId = _marketItems.length - 1;
-    itemsByPublsiher[publisher].push(itemId);
+    itemsByPublsiher[listedOwner].push(itemId);
     if(isSale) {
-      emit ListToken(publisher, itemId);
+      emit ListItem(itemId, listedOwner, price);
     }
   }
 
-/*
   // unlist the sticker from the market
   function unlistToken(uint256 id) public {
-    address publisher = msg.sender;
-    address market = address(this);
-    uint256 amount = _tokenContract.balanceOf(market, id);
+    address listedOwner = msg.sender;
+    MarketItem memory item = _marketItems[id];
 
     require(
-      _tokenContract.isApprovedForAll(market, publisher),
-      "Market must approve that contract operate"
-    );
-    require(
-      _tokenContract.getPublisher(id) == publisher,
-      "Operator is not publisher"
-    );
-    require(
-      marketItems[id].publisher == publisher,
+      item.listedOwner == listedOwner,
       "This token has not listed"
     );
 
-    _tokenContract.safeTransferFrom(market, publisher, id, amount, "");
-    marketItems[id] = MarketItem(address(0), 0, false);
+    _marketItems[id] = MarketItem(item.token, 0, address(0), 0, false);
+    emit UnListItem(id);
   }
-  */
+
+  // buy token
+  function buyToken(uint256 id, uint256 tip) public payable whenNotPaused notBanned nonReentrant {
+    address market = address(this);
+    address buyer = msg.sender;
+    uint256 receiveEther = msg.value;
+    MarketItem memory item = _marketItems[id];
+    ERC1155 token = item.token;
+    uint256 price = item.price + tip;
+    uint256 amount = token.balanceOf(buyer, item.tokenId);
+    require(
+      receiveEther == price,
+      "Not enough Ether, or too much Ether"
+    );
+    require(
+      amount <= 0,
+      "Buyer can only have one token"
+    );
+    _asyncTransfer(item.listedOwner, price);
+    item.token.safeTransferFrom(market, buyer, item.tokenId, 1, "");
+  }
 
   // set sale flag
   function setSale(uint256 id, bool isSale) public {
-    address publisher = msg.sender;
+    address listedOwner = msg.sender;
 
     require(
-      _marketItems[id].publisher == publisher,
-      "Operater is not publisher"
+      _marketItems[id].listedOwner == listedOwner,
+      "Operater is not listedOwner"
     );
 
     _marketItems[id].isSale = isSale;
@@ -87,17 +130,33 @@ contract LiberMarket is Ownable, Pausable, PullPayment {
 
   // set price
   function setPrice(uint256 id, uint256 price) public {
-    address publisher = msg.sender;
+    address listedOwner = msg.sender;
 
     require(
-      _marketItems[id].publisher == publisher,
-      "Operater is not publisher"
+      _marketItems[id].listedOwner == listedOwner,
+      "Operater is not listedOwner"
     );
 
     _marketItems[id].price = price;
   }
 
-  function getPriceByToken(uint256 id) public view returns(uint256) {
-    return _marketItems[id].price;
+  function getDetailByItemId(uint256 id) public view returns (
+    address,
+    uint256,
+    uint256,
+    uint256,
+    bool
+  ) {
+    ERC1155 token = _marketItems[id].token;
+    address listedOwner = _marketItems[id].listedOwner;
+    uint256 tokenId = _marketItems[id].tokenId;
+    uint256 amount = token.balanceOf(listedOwner, tokenId);
+    return (
+      listedOwner,
+      tokenId,
+      _marketItems[id].price,
+      amount,
+      _marketItems[id].isSale
+    );
   }
 }
