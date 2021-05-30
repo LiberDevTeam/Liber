0a3455f3c38b1aedffec96cbc19fb257036550f5
mecaota <24618982+mecaota@users.noreply.github.com>
2021-05-30 23:10:26 +0900

    remove key noAnnounce from ipfs.ts

--- src/lib/ipfs.ts
+++ src/lib/ipfs.ts
@@ -21,7 +21,6 @@ export const getIpfsNode = async (): Promise<IPFSType> => {
               '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
             ],
             announce: [],
-            noAnnounce: [],
           },
         },
       });
