d02ea8c3d6a40a20230f11fb0fece4188619d90e
Koya Fukushi <fukushi098@gmail.com>
2021-06-01 01:28:41 +0900

    implements place info modal

--- src/pages/places/detail/components/place-detail-header/index.tsx
+++ src/pages/places/detail/components/place-detail-header/index.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import { BackLink } from '~/components/back-link';
 import { Button } from '~/components/button';
+import { CloseButton } from '~/components/close-button';
 import { IconButton } from '~/components/icon-button';
 import { IpfsContent } from '~/components/ipfs-content';
 import { SvgBot as BotIcon } from '~/icons/Bot';
@@ -17,14 +18,17 @@ import { SvgPeople as PeopleIcon } from '~/icons/People';
 import { SvgPersonAdd as InviteIcon } from '~/icons/PersonAdd';
 
 const Root = styled.header`
-  display: flex;
-  align-items: center;
   position: fixed;
   width: 100%;
+  top: 0;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
   background: ${(props) => props.theme.colors.white};
   padding: ${(props) =>
     `${props.theme.space[12]}px 0 ${props.theme.space[2]}px`};
-  top: 0;
   border-bottom: ${(props) => props.theme.border.grayLight.light};
 `;
 
@@ -113,12 +117,39 @@ const LeaveButton = styled(Button)`
   color: ${(props) => props.theme.colors.red};
 `;
 
+const PlaceInfo = styled.div`
+  position: relative;
+  background: white;
+  margin: ${(props) => props.theme.space[2]}px;
+  padding: ${(props) => props.theme.space[4]}px;
+  border-radius: ${(props) => props.theme.radii.large}px;
+  box-shadow: ${(props) => props.theme.shadows[2]};
+`;
+
+const PlaceInfoHeader = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: ${(props) => props.theme.space[4]}px;
+`;
+
+const Description = styled.div`
+  margin-bottom: ${(props) => props.theme.space[2]}px;
+  color: ${(props) => props.theme.colors.secondaryText};
+`;
+
+const StyledCloseButton = styled(CloseButton)`
+  position: absolute;
+  right: ${(props) => props.theme.space[2]}px;
+  top: ${(props) => props.theme.space[2]}px;
+`;
+
 export interface PlaceDetailHeaderProps {
   placeId: string;
   address: string;
   name: string;
   avatarCid: string;
   memberCount: number;
+  description: string;
   onInviteClick: () => void;
   onLeave: () => void;
 }
@@ -130,90 +161,98 @@ export const PlaceDetailHeader: React.FC<PlaceDetailHeaderProps> = React.memo(
     name,
     avatarCid,
     memberCount,
+    description,
     onInviteClick,
     onLeave,
   }) {
+    const [openInfo, setOpenInfo] = useState(false);
     const [openMenu, setOpenMenu] = useState(false);
     const { t } = useTranslation('chat');
 
-    const handleInfoClick = () => {
-      console.log('hello');
-    };
-
     return (
       <Root>
-        <BackLink backTo="/places" />
-        <Avatar cid={avatarCid} fallbackComponent={<AvatarPlaceholder />} />
-        <TitleBox>
-          <Title>{name}</Title>
-          <MemberCount>{memberCount} Members</MemberCount>
-        </TitleBox>
-        <Actions>
-          <IconButton
-            title="Open place info"
-            onClick={handleInfoClick}
-            icon={<InfoIcon width={24} height={24} />}
-          />
-
-          <Dropdown
-            visible={openMenu}
-            onOverlayClick={() => setOpenMenu(false)}
-            overlay={() => (
-              <Menu>
-                <MenuButton
-                  title={t('Invite People')}
-                  shape="square"
-                  variant="text"
-                  onClick={onInviteClick}
-                  text={t('Invite People')}
-                  icon={<InviteIcon />}
-                />
-                <MenuButton
-                  title={t('Edit Chat')}
-                  shape="square"
-                  variant="text"
-                  onClick={onInviteClick}
-                  text={t('Edit Chat')}
-                  icon={<EditIcon />}
-                />
-                <MenuButton
-                  title={t('Manage Bots')}
-                  shape="square"
-                  variant="text"
-                  onClick={onInviteClick}
-                  text={t('Manage Bots')}
-                  icon={<BotIcon />}
-                />
-                <MenuButton
-                  title={t('Manage Maintainers')}
-                  shape="square"
-                  variant="text"
-                  onClick={onInviteClick}
-                  text={t('Manage Maintainers')}
-                  icon={<PeopleIcon />}
-                />
-                <MenuLink to={`/places/${address}/${placeId}/banned-users`}>
-                  <PeopleIcon />
-                  {t('Banned Users')}
-                </MenuLink>
-                <LeaveButton
-                  title={t('Leave Chat')}
-                  shape="square"
-                  variant="text"
-                  text={t('Leave Chat')}
-                  onClick={onLeave}
-                  icon={<LeaveIcon />}
-                />
-              </Menu>
-            )}
-          >
+        <Container>
+          <BackLink backTo="/places" />
+          <Avatar cid={avatarCid} fallbackComponent={<AvatarPlaceholder />} />
+          <TitleBox>
+            <Title>{name}</Title>
+            <MemberCount>{memberCount} Members</MemberCount>
+          </TitleBox>
+          <Actions>
             <IconButton
-              icon={<MenuIcon width={24} height={24} />}
-              title="Open menu"
-              onClick={() => setOpenMenu(!openMenu)}
+              title="Open place info"
+              onClick={() => setOpenInfo(!openInfo)}
+              icon={<InfoIcon width={24} height={24} />}
             />
-          </Dropdown>
-        </Actions>
+
+            <Dropdown
+              visible={openMenu}
+              onOverlayClick={() => setOpenMenu(false)}
+              overlay={() => (
+                <Menu>
+                  <MenuButton
+                    title={t('Invite People')}
+                    shape="square"
+                    variant="text"
+                    onClick={onInviteClick}
+                    text={t('Invite People')}
+                    icon={<InviteIcon />}
+                  />
+                  <MenuButton
+                    title={t('Edit Chat')}
+                    shape="square"
+                    variant="text"
+                    onClick={onInviteClick}
+                    text={t('Edit Chat')}
+                    icon={<EditIcon />}
+                  />
+                  <MenuButton
+                    title={t('Manage Bots')}
+                    shape="square"
+                    variant="text"
+                    onClick={onInviteClick}
+                    text={t('Manage Bots')}
+                    icon={<BotIcon />}
+                  />
+                  <MenuLink to={`/places/${address}/${placeId}/banned-users`}>
+                    <PeopleIcon />
+                    {t('Banned Users')}
+                  </MenuLink>
+                  <LeaveButton
+                    title={t('Leave Chat')}
+                    shape="square"
+                    variant="text"
+                    text={t('Leave Chat')}
+                    onClick={onLeave}
+                    icon={<LeaveIcon />}
+                  />
+                </Menu>
+              )}
+            >
+              <IconButton
+                icon={<MenuIcon width={24} height={24} />}
+                title="Open menu"
+                onClick={() => setOpenMenu(!openMenu)}
+              />
+            </Dropdown>
+          </Actions>
+        </Container>
+        {openInfo && (
+          <PlaceInfo>
+            <StyledCloseButton onClick={() => setOpenInfo(false)} />
+            <PlaceInfoHeader>
+              <Avatar
+                cid={avatarCid}
+                fallbackComponent={<AvatarPlaceholder />}
+              />
+              <TitleBox>
+                <Title>{name}</Title>
+                <MemberCount>{memberCount} Members</MemberCount>
+              </TitleBox>
+            </PlaceInfoHeader>
+            <Description>{description}</Description>
+          </PlaceInfo>
+        )}
       </Root>
     );
   }
--- src/pages/places/detail/index.tsx
+++ src/pages/places/detail/index.tsx
@@ -180,11 +180,10 @@ export interface FormValues {
 }
 
 export const ChatDetail: React.FC = React.memo(function ChatDetail() {
-  const { placeId, address } =
-    useParams<{
-      placeId: string;
-      address: string;
-    }>();
+  const { placeId, address } = useParams<{
+    placeId: string;
+    address: string;
+  }>();
   const place = useSelector(selectPlaceById(placeId));
   const messages = useSelector(selectPlaceMessagesByPlaceId(placeId));
   const userIds = arrayUniq(messages.map((m) => m.uid));
@@ -297,6 +296,7 @@ export const ChatDetail: React.FC = React.memo(function ChatDetail() {
               setOpen(true);
             }}
             memberCount={23}
+            description={place.description}
             onLeave={() => {
               dispatch(removePlace({ placeId: place.id }));
               dispatch(push('/places'));
