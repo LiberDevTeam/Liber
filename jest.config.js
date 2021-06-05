97c92326d4bc1d09b97a6519e49f2fe98f3fd00b
Ryo Narita <mail.ryonarita@gmail.com>
2021-06-05 23:11:02 +0900

    Add place slice test

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
-  setupFiles: ['react-app-polyfill/jsdom'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
   ],
   testEnvironment: 'jsdom',
-  testRunner: '/Users/koya/development/jlua/node_modules/jest-circus/runner.js',
   transform: {
     '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
     '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)':
-      '<rootDir>/config/jest/fileTransform.js',
+      '<rootDir>/jest/fileTransform.js',
   },
   transformIgnorePatterns: [
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
@@ -23,6 +20,7 @@ module.exports = {
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '~/(.*)': '<rootDir>/src/$1',
   },
   moduleFileExtensions: [
     'web.js',
---
+++ src/lib/db/__mocks__/place.ts
@@ -0,0 +1,42 @@
+import { Place, PlacePermissions } from '~/state/places/type';
+
+let mockData: DBData = {};
+
+interface KeyValueStoreMock {
+  get(key: string): unknown;
+}
+
+interface DBData {
+  [key: string]: unknown;
+}
+
+export const readPlaceFromDB = (kv: KeyValueStoreMock): Place => {
+  return {
+    id: kv.get('id') as string,
+    name: kv.get('name') as string,
+    avatarCid: kv.get('avatarCid') as string,
+    description: kv.get('description') as string,
+    feedAddress: kv.get('feedAddress') as string,
+    keyValAddress: kv.get('keyValAddress') as string,
+    createdAt: kv.get('createdAt') as number,
+    timestamp: kv.get('timestamp') as number,
+    passwordRequired: kv.get('passwordRequired') as boolean,
+    category: kv.get('category') as number,
+    messageIds: [],
+    unreadMessages: [],
+    permissions: kv.get('permissions') as PlacePermissions,
+    readOnly: kv.get('readOnly') as boolean,
+    bannedUsers: kv.get('bannedUsers') as string[],
+  };
+};
+export const connectPlaceKeyValue = (): KeyValueStoreMock => {
+  return {
+    get: (key: string) => {
+      return mockData[key];
+    },
+  };
+};
+
+export const __setMockData = (data: DBData): void => {
+  mockData = data;
+};
