ed1780222c52667239015b379971983fbbfbbf9c
mecaota <24618982+mecaota@users.noreply.github.com>
2021-06-24 20:50:43 +0900

    make Ethereum Chain id enum

--- src/lib/web3Provider.ts
+++ src/lib/web3Provider.ts
@@ -1,20 +1,52 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
-//import { NetworkConnector } from "@web3-react/network-connector";
+import { NetworkConnector } from '@web3-react/network-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 import web3 from 'web3';
 //import { WalletLinkConnector } from "@web3-react/walletlink-connector";
 import type { provider } from 'web3-core';
 
+/* Ethereum ChainID Enum */
+export enum EthereumChain {
+  'MAIN' = 1,
+  'ROPSTEN' = 3,
+  'RINKEBY' = 4,
+  'KOVAN' = 42,
+}
+
 /* connector */
+const SUPPORT_CHAINIDS = [
+  EthereumChain.MAIN,
+  EthereumChain.ROPSTEN,
+  EthereumChain.RINKEBY,
+  EthereumChain.KOVAN,
+];
+
 export const injectedConnecter = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42],
+  supportedChainIds: SUPPORT_CHAINIDS,
 });
-/* temporary mock parameters
+
+console.log(
+  'test',
+  SUPPORT_CHAINIDS.reduce((accm, chainId): { [chainId: number]: string } => {
+    return {
+      ...accm,
+      [chainId]: `https://${process.env.INFURA_ENDPOINT}${process.env.INFURA_ID}`,
+    };
+  }, {})
+);
+
 export const networkConnector = new NetworkConnector({
-  urls: { 1: "https://mainnet.infura.io/v3/example" },
-  defaultChainId: 1,
+  urls: SUPPORT_CHAINIDS.reduce((accm, chainId): {
+    [chainId: number]: string;
+  } => {
+    return {
+      ...accm,
+      [chainId]: `https://${process.env.INFURA_ENDPOINT}${process.env.INFURA_ID}`,
+    };
+  }, {}),
+  defaultChainId: EthereumChain.ROPSTEN,
 });
-*/
+
 export const walletConnecter = new WalletConnectConnector({
   infuraId: process.env.INFURA_ID,
 });
