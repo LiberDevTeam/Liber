f1af2e4e5b9f5327eb499aeee860725b923a0d6e
mecaota <24618982+mecaota@users.noreply.github.com>
2021-06-29 20:45:11 +0900

    organize types files

--- src/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'prettier-bytes';
-declare module 'ipfs-http-response' {
-  import { IPFS } from 'ipfs';
-  export function getResponse(ipfsNode: IPFS, ipfsPath: string): Response;
-}
