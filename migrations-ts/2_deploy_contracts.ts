c44f57458d2dd4d1eb6b04e1e028e2078f7497a4
mecaota <24618982+mecaota@users.noreply.github.com>
2021-06-30 23:50:46 +0900

    fix migrate task

--- migrations-ts/2_deploy_contracts.ts
+++ migrations-ts/2_deploy_contracts.ts
@@ -1,6 +1,8 @@
 const LiberSticker = artifacts.require('LiberSticker');
+const LiberMarket = artifacts.require('LiberMarket');
 
 module.exports = function (deployer: Truffle.Deployer) {
   // Use deployer to state migration tasks.
-  deployer.deploy(LiberSticker);
+  deployer.deploy(LiberMarket);
+  deployer.deploy(LiberSticker, LiberMarket.address);
 } as Truffle.Migration;
