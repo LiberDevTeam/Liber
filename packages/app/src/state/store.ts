a747f6dcb383abfe396e9ee6486d9c52b075a39f
Ryo Narita <mail.ryonarita@gmail.com>
2021-08-13 14:10:39 +0900

    Show stack trace when dispatched rejected action

--- packages/app/src/state/store.ts
+++ packages/app/src/state/store.ts
@@ -1,5 +1,13 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import {
+  Action,
+  AnyAction,
+  configureStore,
+  isRejected,
+  Middleware,
+  ThunkAction,
+} from '@reduxjs/toolkit';
 import { StoreEnhancer } from 'redux';
+import { createLogger } from 'redux-logger';
 import devToolsEnhancer from 'remote-redux-devtools';
 import { AppDB, appDB } from '~/lib/db';
 import { reducers } from './reducers';
@@ -11,10 +19,36 @@ if (process.env.ENABLE_REMOTE_DEBUG && !window.devToolsExtension) {
   enhancers.push(devToolsEnhancer({ realtime: true, port: 8000 }));
 }
 
+const additionalMiddlewares: Middleware[] = [];
+
+if (process.env.NODE_ENV === 'development') {
+  const check = isRejected();
+  const logger = createLogger({
+    level: {
+      action: 'log',
+      error: 'error',
+      prevState: false,
+      nextState: false,
+    },
+    predicate: (_, action) => check(action),
+    logger: {
+      log: (_: unknown, _0: unknown, action: AnyAction) => {
+        if (check(action)) {
+          console.error(action.type);
+          console.error(action.error.stack);
+        }
+      },
+    },
+  });
+  additionalMiddlewares.push(logger);
+}
+
 export const store = configureStore({
   reducer: reducers,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ thunk: { extraArgument: { db: appDB } } }),
+    getDefaultMiddleware({ thunk: { extraArgument: { db: appDB } } }).concat(
+      additionalMiddlewares
+    ),
   enhancers,
 });
 
