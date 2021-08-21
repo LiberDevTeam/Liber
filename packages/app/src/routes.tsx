14df9e4b473140f672a7daf98839faef6615c19a
kyfk <fukushi098@gmail.com>
2021-08-21 16:19:21 +0900

    delete ga tags (#334)

--- packages/app/src/routes.tsx
+++ packages/app/src/routes.tsx
@@ -28,7 +28,6 @@ import { StickerDetailPage } from './pages/stickers/detail';
 import { StickerEditPage } from './pages/stickers/edit';
 import { StickersPage } from './pages/stickers/index';
 import { StickerNewPage } from './pages/stickers/new';
-import { TrackerProvider } from './state/contexts/tracker';
 
 export const Routes: React.FC = memo(function Routes() {
   const dispatch = useAppDispatch();
@@ -46,132 +45,126 @@ export const Routes: React.FC = memo(function Routes() {
 
   return (
     <Router history={history}>
-      <TrackerProvider>
-        {/* your usual react-router-dom v4/v5 routing */}
-        <Switch>
-          <Route exact path="/" render={() => <HomePage />} />
+      {/* your usual react-router-dom v4/v5 routing */}
+      <Switch>
+        <Route exact path="/" render={() => <HomePage />} />
 
-          <Route exact path="/places/new" render={() => <NewPlace />} />
-          <Route exact path="/places" render={() => <Places />} />
-          <Route
-            exact
-            path="/places/:address/:placeId/banned-users"
-            render={() => <BannedUsers />}
-          />
+        <Route exact path="/places/new" render={() => <NewPlace />} />
+        <Route exact path="/places" render={() => <Places />} />
+        <Route
+          exact
+          path="/places/:address/:placeId/banned-users"
+          render={() => <BannedUsers />}
+        />
 
-          <Route
-            exact
-            path="/places/:address/:placeId/manage-bots"
-            render={() => <ManageBotsPage />}
-          />
-          <Route
-            exact
-            path="/places/:address/:placeId/edit"
-            render={() => <PlaceEdit />}
-          />
-          <Route
-            exact
-            path="/places/:address/:placeId/:swarmKey?"
-            render={() => <ChatDetail />}
-          />
+        <Route
+          exact
+          path="/places/:address/:placeId/manage-bots"
+          render={() => <ManageBotsPage />}
+        />
+        <Route
+          exact
+          path="/places/:address/:placeId/edit"
+          render={() => <PlaceEdit />}
+        />
+        <Route
+          exact
+          path="/places/:address/:placeId/:swarmKey?"
+          render={() => <ChatDetail />}
+        />
 
-          <Route exact path="/explore" render={() => <Explore />} />
+        <Route exact path="/explore" render={() => <Explore />} />
 
-          <Route exact path="/profile" render={() => <ProfilePage />} />
-          <Route
-            exact
-            path="/profile/edit"
-            render={() => <ProfileEditPage />}
-          />
+        <Route exact path="/profile" render={() => <ProfilePage />} />
+        <Route exact path="/profile/edit" render={() => <ProfileEditPage />} />
 
-          <Route
-            exact
-            path="/marketplace/:type?"
-            render={() => (
-              <WithWeb3>
-                <MarketplacePage />
-              </WithWeb3>
-            )}
-          />
+        <Route
+          exact
+          path="/marketplace/:type?"
+          render={() => (
+            <WithWeb3>
+              <MarketplacePage />
+            </WithWeb3>
+          )}
+        />
 
-          <Route exact path="/bots" render={() => <Bots />} />
-          <Route
-            exact
-            path="/bots/new"
-            render={() => (
-              <WithWeb3>
-                <BotNewPage />
-              </WithWeb3>
-            )}
-          />
-          <Route
-            exact
-            path="/bots/:address/:botId/edit"
-            render={() => (
-              <WithWeb3>
-                <BotEditPage />
-              </WithWeb3>
-            )}
-          />
-          <Route
-            exact
-            path="/bots/:address/:botId"
-            render={() => (
-              <WithWeb3>
-                <BotDetailPage />
-              </WithWeb3>
-            )}
-          />
+        <Route exact path="/bots" render={() => <Bots />} />
+        <Route
+          exact
+          path="/bots/new"
+          render={() => (
+            <WithWeb3>
+              <BotNewPage />
+            </WithWeb3>
+          )}
+        />
+        <Route
+          exact
+          path="/bots/:address/:botId/edit"
+          render={() => (
+            <WithWeb3>
+              <BotEditPage />
+            </WithWeb3>
+          )}
+        />
+        <Route
+          exact
+          path="/bots/:address/:botId"
+          render={() => (
+            <WithWeb3>
+              <BotDetailPage />
+            </WithWeb3>
+          )}
+        />
 
-          <Route
-            exact
-            path="/stickers/new"
-            render={() => (
-              <WithWeb3>
-                <StickerNewPage />
-              </WithWeb3>
-            )}
-          />
-          <Route
-            exact
-            path="/stickers/:address/:stickerId"
-            render={() => (
-              <WithWeb3>
-                <StickerDetailPage />
-              </WithWeb3>
-            )}
-          />
-          <Route
-            exact
-            path="/stickers/:address/:stickerId/edit"
-            render={() => (
-              <WithWeb3>
-                <StickerEditPage />
-              </WithWeb3>
-            )}
-          />
-          <Route
-            exact
-            path="/stickers"
-            render={() => (
-              <WithWeb3>
-                <StickersPage />
-              </WithWeb3>
-            )}
-          />
+        <Route
+          exact
+          path="/stickers/new"
+          render={() => (
+            <WithWeb3>
+              <StickerNewPage />
+            </WithWeb3>
+          )}
+        />
+        <Route
+          exact
+          path="/stickers/:address/:stickerId"
+          render={() => (
+            <WithWeb3>
+              <StickerDetailPage />
+            </WithWeb3>
+          )}
+        />
+        <Route
+          exact
+          path="/stickers/:address/:stickerId/edit"
+          render={() => (
+            <WithWeb3>
+              <StickerEditPage />
+            </WithWeb3>
+          )}
+        />
+        <Route
+          exact
+          path="/stickers"
+          render={() => (
+            <WithWeb3>
+              <StickersPage />
+            </WithWeb3>
+          )}
+        />
 
-          <Route exact path="/settings" render={() => <SettingsPage />} />
+        <Route exact path="/settings" render={() => <SettingsPage />} />
 
-          <Route
-            exact
-            path="/notifications"
-            render={() => <NotificationsPage />}
-          />
+        <Route
+          exact
+          path="/notifications"
+          render={() => <NotificationsPage />}
+        />
 
-          <Route exact path="/404" render={() => <NotFoundPage />} />
-          <Route render={() => <NotFoundPage />} />
-        </Switch>
-      </TrackerProvider>
+        <Route exact path="/404" render={() => <NotFoundPage />} />
+        <Route render={() => <NotFoundPage />} />
+      </Switch>
     </Router>
   );
 });
