768e56e35e77680f8acf0bce5f4ad4d6f7082f11
Ryo Narita <mail.ryonarita@gmail.com>
2021-08-29 23:20:20 +0900

    Add bootstraps

--- packages/app/src/lib/ipfs.ts
+++ packages/app/src/lib/ipfs.ts
@@ -20,9 +20,17 @@ export const getIpfsNode = async (): Promise<IPFSType> => {
               // '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
               // '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
               // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
-              '/dns4/salty-temple-29450.herokuapp.com/tcp/443/wss/p2p-webrtc-star/',
+              '/dns4/salty-temple-29450.herokuapp.com/tcp/443/wss/p2p-webrtc-star',
             ],
           },
+          Bootstrap: [
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+            '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+            '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+            '/ip4/104.131.131.82/udp/4001/quic/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+          ],
         },
       });
     }
