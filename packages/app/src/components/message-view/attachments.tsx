7715c7a432ae8a975d0bde024b60a3b61271f19c
Ryo Narita <mail.ryonarita@gmail.com>
2021-09-15 01:08:33 +0900

    Show image when click image at chat messages

--- packages/app/src/components/message-view/attachments.tsx
+++ packages/app/src/components/message-view/attachments.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { IpfsContent } from '~/components/ipfs-content';
+import { Modal } from '~/components/modal';
 import { Loading } from '~/icons/loading';
 
 const Attachment = styled(IpfsContent)`
   max-height: 100px;
+`;
+
+const OpenedImage = styled(IpfsContent)`
+  max-height: 80vh;
+  max-width: 80vw;
 `;
 
 const Root = styled.div`
@@ -27,6 +33,11 @@ interface Props {
 export const Attachments: React.FC<Props> = React.memo(function Attachments({
   attachments,
 }) {
+  const [showImageCID, setShowImageCID] = useState<string | null>(null);
+  const handleClickAttachment = useCallback((cid: string) => {
+    setShowImageCID(cid);
+  }, []);
+
   if (!attachments) {
     return null;
   }
@@ -36,18 +47,24 @@ export const Attachments: React.FC<Props> = React.memo(function Attachments({
   }
 
   return (
-    <Root>
-      {attachments.map((cid) => (
-        <Attachment
-          key={`${cid}`}
-          cid={cid}
-          fallbackComponent={
-            <AttachmentLoadingWrapper>
-              <Loading width={56} height={56} />
-            </AttachmentLoadingWrapper>
-          }
-        />
-      ))}
-    </Root>
+    <>
+      <Root>
+        {attachments.map((cid) => (
+          <Attachment
+            key={`${cid}`}
+            cid={cid}
+            onImageClick={handleClickAttachment}
+            fallbackComponent={
+              <AttachmentLoadingWrapper>
+                <Loading width={56} height={56} />
+              </AttachmentLoadingWrapper>
+            }
+          />
+        ))}
+      </Root>
+      <Modal open={Boolean(showImageCID)} onClose={() => setShowImageCID(null)}>
+        {showImageCID ? <OpenedImage cid={showImageCID} /> : null}
+      </Modal>
+    </>
   );
 });
