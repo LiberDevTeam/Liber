7342eb34f77e98df8e989a15ebe0e04cb9d37090
Ryo Narita <mail.ryonarita@gmail.com>
2021-10-11 23:55:16 +0900

    Remove contracts package

--- packages/contracts/migrations-ts/1_initial_migration.ts
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require('Migrations');
-
-module.exports = function (deployer: Truffle.Deployer) {
-  deployer.deploy(Migrations);
-} as Truffle.Migration;
-
-export {};
