d53d9cbb137a602637be756ddbf72c8eaf2c3e11
Ryo Narita <mail.ryonarita@gmail.com>
2021-10-12 23:17:52 +0900

    Remove contracts package (#536)

--- packages/contracts/migrations-ts/2_deploy_contracts.ts
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require('Migrations');
-const LiberSticker = artifacts.require('LiberSticker');
-const LiberMarket = artifacts.require('LiberMarket');
-
-module.exports = async function (deployer: Truffle.Deployer) {
-  // Use deployer to state migration tasks.
-  await deployer.deploy(LiberMarket);
-  await deployer.deploy(LiberSticker, LiberMarket.address);
-} as Truffle.Migration;
